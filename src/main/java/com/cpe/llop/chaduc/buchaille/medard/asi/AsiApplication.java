2a802ad83beb7a6739f9587bf337ebae7dcfba1f
Metean <anthelme.buchaille@gmail.com>
2023-05-24 12:07:06 +0200

    added user and test controller

--- src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/AsiApplication.java
+++ src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/AsiApplication.java
@@ -9,8 +9,6 @@ public class AsiApplication {
     public static void main(String[] args) {
         SpringApplication.run(AsiApplication.class, args);
     }
-
-
 }
 
 
---
+++ src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/controllers/UserController.java
@@ -0,0 +1,43 @@
+package com.cpe.llop.chaduc.buchaille.medard.asi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    //private UserService userService;
+
+    //@Autowired
+    //public UserController(UserService userService) {
+    //    this.userService = userService;
+    //}
+
+    @GetMapping("/summary")
+    public void/*UserSummaryResponse*/ getUserSummary(@RequestParam("userId") String userId) {
+        // Implement getUserSummary logic using the userService
+        // Return the user summary response
+    }
+
+    @PostMapping("/add")
+    public void addUser(/*@RequestBody AddUserRequest addUserRequest*/) {
+        // Implement addUser logic using the userService
+    }
+
+    @PostMapping("/money")
+    public void setUserMoney(/*@RequestBody SetUserMoneyRequest setUserMoneyRequest*/) {
+        // Implement setUserMoney logic using the userService
+    }
+
+    @PostMapping("/card/add")
+    public void addUserCard(/*@RequestBody AddUserCardRequest addUserCardRequest*/) {
+        // Implement addUserCard logic using the userService
+    }
+
+    @PostMapping("/card/remove")
+    public void removeUserCard(/*@RequestBody RemoveUserCardRequest removeUserCardRequest*/) {
+        // Implement removeUserCard logic using the userService
+    }
+}
+
