be346a57090321cdf304953584a30adf71373841
Medard Mathis <mathis.medard@infomaniak.com>
2023-05-24 15:26:41 +0200

    fix: remove redundant repository

--- src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cpe.llop.chaduc.buchaille.medard.asi.repository;
-
-import com.cpe.llop.chaduc.buchaille.medard.asi.repositories.Entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    public User findByUsername(String username);
-}
