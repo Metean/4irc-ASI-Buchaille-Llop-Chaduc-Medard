e0a4823c014fe90bdc78604c1c4ffbbf8394ff43
Metean <anthelme.buchaille@gmail.com>
2023-05-24 14:18:15 +0200

    fix: removed @autowired cause we're using constructor DI, Added methods in the user interface and implementation

--- src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/services/UserService.java
+++ src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/services/UserService.java
@@ -1,4 +1,16 @@
 package com.cpe.llop.chaduc.buchaille.medard.asi.services;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public interface UserService {
+    public void /*UserSummaryResponse*/ getUserSummary(@RequestParam("userId") String userId);
+
+    public void addUser(/*@RequestBody AddUserRequest addUserRequest*/);
+
+    public void setUserMoney(/*@RequestBody SetUserMoneyRequest setUserMoneyRequest*/);
+
+    public void addUserCard(/*@RequestBody AddUserCardRequest addUserCardRequest*/);
+
+    public void removeUserCard(/*@RequestBody RemoveUserCardRequest removeUserCardRequest*/);
 }
--- src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/services/UserServiceImpl.java
+++ src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/services/UserServiceImpl.java
@@ -1,8 +1,29 @@
 package com.cpe.llop.chaduc.buchaille.medard.asi.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class UserServiceImpl implements UserService {
     //TODO: report the logic from the user controller here, (with the connection to the database)
+    public void/*UserSummaryResponse*/ getUserSummary(@RequestParam("userId") String userId) {
+
+    }
+
+    public void addUser(/*@RequestBody AddUserRequest addUserRequest*/) {
+
+    }
+
+    public void setUserMoney(/*@RequestBody SetUserMoneyRequest setUserMoneyRequest*/) {
+
+    }
+
+    public void addUserCard(/*@RequestBody AddUserCardRequest addUserCardRequest*/) {
+
+    }
+
+    public void removeUserCard(/*@RequestBody RemoveUserCardRequest removeUserCardRequest*/) {
+
+    }
 }
