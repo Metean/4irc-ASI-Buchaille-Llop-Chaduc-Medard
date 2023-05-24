897e6984ba76326bfc1db36aafb7de90f0b846d9
Metean <anthelme.buchaille@gmail.com>
2023-05-24 14:12:21 +0200

    service interface for dependency injection

---
+++ src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/services/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cpe.llop.chaduc.buchaille.medard.asi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    //TODO: report the logic from the user controller here, (with the connection to the database)
+}
