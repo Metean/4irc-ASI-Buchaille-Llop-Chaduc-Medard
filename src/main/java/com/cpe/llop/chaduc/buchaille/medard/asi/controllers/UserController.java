6ada62757fb13136554ffeaf8891e9bbe5711ff8
Metean <anthelme.buchaille@gmail.com>
2023-05-24 14:18:15 +0200

    fix: removed @autowired cause we're using constructor DI, Added methods in the user interface and implementation

--- src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/controllers/UserController.java
+++ src/main/java/com/cpe/llop/chaduc/buchaille/medard/asi/controllers/UserController.java
@@ -10,7 +10,6 @@ public class UserController {
 
     private UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
