2b2aaa54883b03c73db195c5ae757cc137a92dd3
Youreastonefox <louisa.chd@gmail.com>
2023-05-30 10:34:28 +0200

    Amélioration form register

--- src/main/resources/static/app/src/components/user/Register.vue
+++ src/main/resources/static/app/src/components/user/Register.vue
@@ -1,30 +1,20 @@
 <template>
 	<div class="register">
-		<div>INSCRIPTION</div>
-
-		<div class="register__form border-secondary ">
-			<b-form @submit="onSubmit" @reset="onReset" v-if="show">
-				<b-form-group id="input-group-1" label="Email address:" label-for="input-1" description="We'll never share your email with anyone else.">
-					<b-form-input id="input-1" v-model="form.email" type="email" placeholder="Enter email" required></b-form-input>
+		<div class="register__form">
+			<b-form @submit="onSubmit">
+				<b-form-group id="username" label="Nom d'utilisateur :">
+					<b-form-input id="username" v-model="form.username" type="text" placeholder="" required></b-form-input>
 				</b-form-group>
 
-				<b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-					<b-form-input id="input-2" v-model="form.name" placeholder="Enter name" required></b-form-input>
+				<b-form-group id="mail" label="Adresse mail :">
+					<b-form-input id="mail" v-model="form.mail" type="email" placeholder="" required></b-form-input>
 				</b-form-group>
 
-				<b-form-group id="input-group-3" label="Food:" label-for="input-3">
-					<b-form-select id="input-3" v-model="form.food" :options="foods" required></b-form-select>
+				<b-form-group id="password" label="Mot de passe :">
+					<b-form-input id="password" v-model="form.password" type="password" placeholder="" required></b-form-input>
 				</b-form-group>
 
-				<b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-					<b-form-checkbox-group v-model="form.checked" id="checkboxes-4" :aria-describedby="ariaDescribedby">
-						<b-form-checkbox value="me">Check me out</b-form-checkbox>
-						<b-form-checkbox value="that">Check that out</b-form-checkbox>
-					</b-form-checkbox-group>
-				</b-form-group>
-
-				<b-button type="submit" variant="primary">Submit</b-button>
-				<b-button type="reset" variant="danger">Reset</b-button>
+				<b-button type="submit" variant="primary">Inscription</b-button>
 			</b-form>
 		</div>
 
@@ -45,31 +35,14 @@ import { Options, Vue } from 'vue-class-component'
 export default class Register extends Vue {
 	msg!: string
 	form = {
-		email: '',
-		name: '',
-		food: null,
-		checked: [],
+		username: '',
+		mail: '',
+		password: '',
 	}
-	foods = [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn']
-	show = true
 
 	onSubmit(event: any) {
 		event.preventDefault()
 		alert(JSON.stringify(this.form))
-	}
-
-	onReset(event: any) {
-		event.preventDefault()
-		// Reset our form values
-		this.form.email = ''
-		this.form.name = ''
-		this.form.food = null
-		this.form.checked = []
-		// Trick to reset/clear native browser form validation state
-		this.show = false
-		this.$nextTick(() => {
-			this.show = true
-		})
 	}
 }
 </script>
@@ -81,9 +54,13 @@ export default class Register extends Vue {
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+	padding-top: 32px;
 
 	&__form {
 		width: 80%;
+		border: 1px solid #c5c5c5;
+		border-radius: 10px;
+		padding: 24px 32px;
 	}
 }
 </style>
